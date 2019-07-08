b0e8429110f168d36a56f77254871902dc9e4a38
Cloud User <ec2-user@ip-172-31-36-29.us-east-2.compute.internal>
2019-07-08 07:10:29 +0000

    .c files modified

--- big3.c
+++ big3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-big();
+
 void big()
 {
     double n1, n2, n3;
     printf("Enter three numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
     if( n1>=n2 && n1>=n3)
-        printf("%.2lf is the largest number.", n1\n);
+        printf("%.2lf is the largest number.\n", n1);
     else if (n2>=n1 && n2>=n3)
-        printf("%.2lf is the largest number.", n2\n);
+        printf("%.2lf is the largest number.\n", n2);
     else
-        printf("%.2lf is the largest number.", n3\n);
+        printf("%.2lf is the largest number.\n", n3);
     }
